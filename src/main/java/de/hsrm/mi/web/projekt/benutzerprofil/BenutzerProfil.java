a06f25910cc9761c60dc1f517213241e86e06460
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-04-28 09:52:10 +0200

    init

---
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
@@ -0,0 +1,124 @@
+package de.hsrm.mi.web.projekt.benutzerprofil;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BenutzerProfil {
+    String name;
+    LocalDate geburtsdatum;
+    String adresse;
+    String email;
+    String lieblingsfarbe;
+    String interessen;
+
+    public BenutzerProfil(){
+        this.name = "";
+        this.geburtsdatum = LocalDate.of(1,1,1);
+        this.adresse = "";
+        this.email = "";
+        this.lieblingsfarbe = "";
+        this.interessen = "";
+    }
+
+    public String toString(){
+        return 
+            "Name: "+ this.name +
+            "\nGeburtsdatum: "+ this.geburtsdatum +
+            "\nAdresse: " + this.adresse +
+            "\nE-Mail: " + this.email +
+            "\nLieblingsfarbe: " + this.lieblingsfarbe +
+            "\nInteressen: " + this.interessen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getGeburtsdatum() {
+        return geburtsdatum;
+    }
+
+    public void setGeburtsdatum(LocalDate geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLieblingsfarbe() {
+        return lieblingsfarbe;
+    }
+
+    public void setLieblingsfarbe(String lieblingsfarbe) {
+        this.lieblingsfarbe = lieblingsfarbe;
+    }
+
+    public String getInteressen() {
+        return interessen;
+    }
+
+    public void setInteressen(String interessen) {
+        this.interessen = interessen;
+    }
+
+    public List<String> getInteressenListe(){
+        List<String> interessenListe = new ArrayList<>();
+        if (!this.interessen.equals("")){
+            interessenListe = Arrays.asList(this.interessen.split(" , "));
+        }
+        return interessenListe;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((geburtsdatum == null) ? 0 : geburtsdatum.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BenutzerProfil other = (BenutzerProfil) obj;
+        if (geburtsdatum == null) {
+            if (other.geburtsdatum != null)
+                return false;
+        } else if (!geburtsdatum.equals(other.geburtsdatum))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+    
+    
+}
