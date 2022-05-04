843fe6774c4135ea576afc193d82752346a4f184
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-05-04 13:41:23 +0200

    bearbeiten - Seite erweitert, profileditor verbessert und email attribut korrigiert

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilController.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -25,6 +27,7 @@ public class BenutzerprofilController {
         bp.setEmail("ha@lo.lo");
         bp.setAdresse("Goldgasse 10, 65185 Wiesbaden");
         bp.setInteressen("schwimmen, lesen, schreiben");
+        bp.setLieblingsfarbe("#000000");
         m.addAttribute("profil", bp);
     }
 
@@ -34,7 +37,7 @@ public class BenutzerprofilController {
         m.addAttribute("name", profil.getName());
         m.addAttribute("geburtsdatum", profil.getGeburtsdatum());
         m.addAttribute("adresse", profil.getAdresse());
-        m.addAttribute("eMail", profil.getEmail());
+        m.addAttribute("email", profil.getEmail());
         m.addAttribute("lieblingsfarbe", profil.getLieblingsfarbe());
         m.addAttribute("interessenliste", profil.getInteressenListe());
         
@@ -43,6 +46,18 @@ public class BenutzerprofilController {
 
     @GetMapping("/benutzerprofil/bearbeiten")
     public String getProfileditor(@ModelAttribute("profil") BenutzerProfil profil, Model m){
+        m.addAttribute("name", profil.getName());
+        m.addAttribute("geburtsdatum", profil.getGeburtsdatum());
+        m.addAttribute("adresse", profil.getAdresse());
+        m.addAttribute("email", profil.getEmail());
+        m.addAttribute("lieblingsfarbe", profil.getLieblingsfarbe());
+        m.addAttribute("interessenliste", profil.getInteressen());
         return "benutzerprofil/profileditor";
+    }
+
+    @PostMapping("/benutzerprofil/bearbeiten")
+    public String postProfil(@ModelAttribute("profil") BenutzerProfil profil, Model m){
+        
+        return "redirect:/benutzerprofil";
     }
 }
