48cd48326d25320cac4346dd130ac2610dbc0917
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-05-24 20:29:22 +0200

    BenutzerProfilService erweitert für Angebote

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerprofilServiceImpl.java
@@ -8,35 +8,39 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import de.hsrm.mi.web.projekt.angebot.Angebot;
+import de.hsrm.mi.web.projekt.angebot.AngebotRepository;
 import de.hsrm.mi.web.projekt.geo.AdressInfo;
 import de.hsrm.mi.web.projekt.geo.GeoService;
 
 @Service
+@Transactional
 public class BenutzerprofilServiceImpl implements BenutzerprofilService{
     Logger logger = LoggerFactory.getLogger(BenutzerprofilService.class);
     
     @Autowired
-    private BenutzerprofilRepository repository;
+    private BenutzerprofilRepository benutzerProfilRepository;
+    @Autowired
+    private AngebotRepository angebotRepository;
     @Autowired
     private GeoService geoService;
 
     @Override
     public BenutzerProfil speichereBenutzerProfil(BenutzerProfil bp) {
-        logger.info(bp.toString());
         List<AdressInfo> adressInfos = geoService.findeAdressInfo(bp.getAdresse());
         if(adressInfos.size() > 0){
             AdressInfo adressInfo = adressInfos.get(0);
             bp.setLat(adressInfo.lat());
             bp.setLon(adressInfo.lon());
         }
-        logger.info(bp.toString());
-        return repository.save(bp);
+        return benutzerProfilRepository.save(bp);
     }
 
     @Override
     public Optional<BenutzerProfil> holeBenutzerProfilMitId(Long id) {
-        for (BenutzerProfil bp: repository.findAll()){
+        for (BenutzerProfil bp: benutzerProfilRepository.findAll()){
             if (bp.getId() == id){
                 return Optional.of(bp);
             }
@@ -46,12 +50,42 @@ public class BenutzerprofilServiceImpl implements BenutzerprofilService{
 
     @Override
     public List<BenutzerProfil> alleBenutzerProfile() {
-        return repository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        return benutzerProfilRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
     }
 
     @Override
     public void loescheBenutzerProfilMitId(Long loesch) {
-        repository.deleteById(loesch);
+        benutzerProfilRepository.deleteById(loesch);
+    }
+
+    @Override
+    public void fuegeAngebotHinzu(long id, Angebot angebot) {
+       List<AdressInfo> adressen = geoService.findeAdressInfo(angebot.getAbholort());
+       if (!adressen.isEmpty()){
+           AdressInfo abholort = adressen.get(0);
+           angebot.setLat(abholort.lat());
+           angebot.setLon(abholort.lon());
+       }else{
+        angebot.setLat(0);
+        angebot.setLon(0);
+       }
+       BenutzerProfil benutzerprofil = holeBenutzerProfilMitId(id).orElseThrow();
+       angebot.setAnbieter(benutzerprofil);
+       benutzerprofil.getAngebote().add(angebot);
+       logger.info("Angebotsanzahl: " + benutzerprofil.getAngebote().size());
+       for (Angebot ang: benutzerprofil.getAngebote()){
+           logger.info(ang.getBeschreibung());
+       }
+    }
+
+    @Override
+    public void loescheAngebot(long id) {
+        BenutzerProfil anbieter = angebotRepository.getById(id).getAnbieter();
+        logger.error("vor remove");
+        anbieter.getAngebote().remove(angebotRepository.getById(id));
+        logger.error("nach remove");
+        angebotRepository.deleteById(id);
+        logger.error("nach deleteById");
     }
     
 }
