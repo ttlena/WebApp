7a3734dc43af851d2494408434935cd36049d943
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-06-13 20:24:14 +0200

    frontend Ordner hinzugefügt

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import GeoLink from '@/components/GeoLink.vue';
+</script>
+
+<template>
+    <main>
+        <GeoLink :lat="52.9849" :lon="9.5794">Visselhövede</GeoLink>
+        <br>
+        <GeoLink :lat="52.9849" :lon="9.5794" :zoom="15" >Visselhövede</GeoLink>
+    </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
