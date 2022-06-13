78d90ba88a52fb696ec287cfa6da98094be871ba
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-06-13 20:25:15 +0200

    GeoLink.vue erstellt

---
+++ frontend/src/components/GeoLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        lat:number,
+        lon:number,
+        zoom: number
+    }>(),
+    {zoom: 17}
+)
+const link = `https://www.openstreetmap.org/#map=${props.zoom}/${props.lat}/${props.lon}`
+</script>
+
+<template>
+    <a target="_blank" :href="link">
+        <slot>Ort</slot>
+    </a>
+</template>
