29b403594bcbc618e1a42e6f7beeece286409c3b
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-06-14 18:06:25 +0200

    AboutView aktualisiert zum Testen

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
     import GeoLink from '@/components/GeoLink.vue';
+    import AngebotListItem from '@/components/AngebotListItem.vue';
+    import AngebotListe from '@/components/AngebotListe.vue';
+    import { useFakeAngebot } from '@/services/useFakeAngebot';
+
+    const { angebote, bietenSimulieren } = useFakeAngebot();
 </script>
 
 <template>
@@ -7,6 +12,19 @@
         <GeoLink :lat="52.9849" :lon="9.5794">Visselhövede</GeoLink>
         <br>
         <GeoLink :lat="52.9849" :lon="9.5794" :zoom="15" >Visselhövede</GeoLink>
+
+        <br><br><br>
+
+        <h3>About...</h3>
+        <div v-for="a in angebote">
+          <AngebotListItem :angebot="a" />
+        </div>
+        <button v-on:click="bietenSimulieren">bieten simulieren</button>
+
+        <br><br>
+
+        <h3>Aktuelle Angebote</h3>
+        <AngebotListe></AngebotListe>
     </main>
 </template>
 
