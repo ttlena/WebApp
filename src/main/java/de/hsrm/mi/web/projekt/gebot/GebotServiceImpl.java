99d18a4fb9a485e4c17cf98285e278511d29bcb9
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-07-01 05:07:28 +0200

    Backend informiert über neue Gebote

--- src/main/java/de/hsrm/mi/web/projekt/gebot/GebotServiceImpl.java
+++ src/main/java/de/hsrm/mi/web/projekt/gebot/GebotServiceImpl.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import de.hsrm.mi.web.projekt.angebot.Angebot;
+import de.hsrm.mi.web.projekt.api.gebot.GetGebotResponseDTO;
 import de.hsrm.mi.web.projekt.benutzerprofil.BenutzerProfil;
 import de.hsrm.mi.web.projekt.benutzerprofil.BenutzerprofilService;
+import de.hsrm.mi.web.projekt.messaging.BackendInfoService;
+import de.hsrm.mi.web.projekt.messaging.BackendOperation;
 
 @Service
 public class GebotServiceImpl implements GebotService{
@@ -19,6 +22,9 @@ public class GebotServiceImpl implements GebotService{
     
     @Autowired
     private BenutzerprofilService benutzerprofilService;
+
+    @Autowired
+    private BackendInfoService backendInfoService;
 
     @Override
     public List<Gebot> findeAlleGebote() {
@@ -43,21 +49,28 @@ public class GebotServiceImpl implements GebotService{
             benutzerprofilService.findeAngebotMitId(angebotid).orElseThrow().setGebot(neuesGebot);
             benutzerprofilService.holeBenutzerProfilMitId(benutzerprofilid).orElseThrow().setGebot(neuesGebot);
             gebotRepository.save(neuesGebot);
+            //Gebot-DTO senden?
+            GetGebotResponseDTO gebotDTO = GetGebotResponseDTO.from(neuesGebot);
+            backendInfoService.sendInfo(String.format("/gebot/%d",gebotDTO.angebotid()), BackendOperation.CREATE, gebotDTO.gebotid());
             return neuesGebot;
         }else{
             gebot.get().setBetrag(betrag);
             gebot.get().setGebotzeitpunkt(LocalDateTime.now());
+            GetGebotResponseDTO gebotDTO = GetGebotResponseDTO.from(gebot.get());
+            backendInfoService.sendInfo(String.format("/gebot/%d",gebotDTO.angebotid()), BackendOperation.UPDATE, gebotDTO.gebotid());
             return gebot.get();
         }
     }
 
     @Override
     public void loescheGebot(long gebotid) {
-        Gebot gebot = gebotRepository.getById(gebotid);
+        Gebot gebot = gebotRepository.findById(gebotid).orElseThrow();
         Angebot angebot = gebot.getAngebot();
         angebot.getGebote().remove(gebot);
         BenutzerProfil bieter = gebot.getGebieter();
         bieter.getGebote().remove(gebot);
+        GetGebotResponseDTO gebotDTO = GetGebotResponseDTO.from(gebot);
+        backendInfoService.sendInfo(String.format("/gebot/%d",gebotDTO.angebotid()), BackendOperation.DELETE, gebotDTO.gebotid());
         gebotRepository.deleteById(gebotid);
     }
     
